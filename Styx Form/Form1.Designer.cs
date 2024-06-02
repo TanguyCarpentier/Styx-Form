61c10bf1b21a6a7d1e556cae14e16b614f344370
Tanguy <carpentiertanguy@gmail.com>
2024-06-02 18:43:08 +0200

    Ajoutez des fichiers projet.

---
+++ Styx Form/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Styx_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.casque_off;
+            pictureBox1.Location = new Point(17, 337);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(74, 72);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(pictureBox1);
+            groupBox1.Location = new Point(171, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(499, 426);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "MenuPrincipal";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(147, 126);
+            button1.Name = "button1";
+            button1.Size = new Size(221, 51);
+            button1.TabIndex = 1;
+            button1.Text = "Jouer";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(147, 205);
+            button2.Name = "button2";
+            button2.Size = new Size(221, 51);
+            button2.TabIndex = 2;
+            button2.Text = "ScoreBoard";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(147, 287);
+            button3.Name = "button3";
+            button3.Size = new Size(221, 51);
+            button3.TabIndex = 3;
+            button3.Text = "Quitter";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            groupBox1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private GroupBox groupBox1;
+        private Button button1;
+        private Button button3;
+        private Button button2;
+    }
+}
